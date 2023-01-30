08db67616c346c4b2f173042f0a3dfe39627c454
MirzaHilmi <exquisitemirza@student.ub.ac.id>
2023-01-30 22:18:18 +0700

    move function into util package

--- models/item.go
+++ models/item.go
@@ -1,9 +1,11 @@
 package models
 
-import "encoding/json"
+import (
+	"encoding/json"
+)
 
-type Book struct {
-	Title string `json:"title" binding:"required"`
-	Years json.Number   `json:"years" binding:"required,number"`
-	Price json.Number   `json:"price" binding:"required,number"`
+type BookEntity struct {
+	Title string      `json:"title" binding:"required"`
+	Years json.Number `json:"years" binding:"required,number"`
+	Price json.Number `json:"price" binding:"required,number"`
 }
