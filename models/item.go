2b009a30219d7c89ad4e11df29a0bf08336c0284
MirzaHilmi <exquisitemirza@student.ub.ac.id>
2023-01-30 18:41:23 +0700

    fix indentation

--- models/item.go
+++ models/item.go
@@ -2,6 +2,6 @@ package models
 
 type Book struct {
 	Title string `json:"title" binding:"required"`
-	Years uint   `json:"years" binding:"required, number"`
-	Price uint   `json:"price" binding:"required, number"`
+	Years uint   `json:"years" binding:"required,number"`
+	Price uint   `json:"price" binding:"required,number"`
 }
