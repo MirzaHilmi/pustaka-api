648175a7fadfe56afd818e6dddd568f15ce0386d
MirzaHilmi <exquisitemirza@student.ub.ac.id>
2023-01-30 16:46:55 +0700

    modified

--- main.go
+++ main.go
@@ -9,10 +9,21 @@ import (
 func main() {
 	router := gin.Default()
 
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"name": "Mirza Hilmi",
-			"Bio": "A Software Developer, Backend Developing and Robotics Enthusiast",
-		})
-	})  // <- Router Path
+	router.GET("/", rootHandler)
+	router.GET("/:id", idHandler)
+	router.Run()
+}
+
+func rootHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"name": "Mirza Hilmi",
+		"Bio": "A Software Developer, Backend Developing and Robotics Enthusiast",
+	})
+}
+
+func idHandler(c *gin.Context) {
+	id := c.Param("id")
+	c.JSON(http.StatusOK, gin.H{
+		"id": id,
+	})
 }
