ca2676618edd4e1238b44bd11daf93866530432a
MirzaHilmi <exquisitemirza@student.ub.ac.id>
2023-01-30 22:18:56 +0700

    import all function from main and handler

---
+++ util/handler.go
@@ -0,0 +1,56 @@
+package util
+
+import (
+	"fmt"
+	"net/http"
+
+	"pustaka-api/models"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+)
+
+func RootHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"name": "Mirza Hilmi",
+		"Bio":  "A Software Developer, Backend Developing and Robotics Enthusiast",
+	})
+}
+
+func IdHandler(c *gin.Context) {
+	id := c.Param("id")
+	c.JSON(http.StatusOK, gin.H{
+		"id": id,
+	})
+}
+
+func QueryHandler(c *gin.Context) {
+	title := c.Query("title")
+	price := c.Query("price")
+
+	c.JSON(http.StatusOK, gin.H{
+		"title": title,
+		"price": price,
+	})
+}
+
+func PostBookHandler(c *gin.Context) {
+	var book models.BookEntity
+
+	if err := c.ShouldBindJSON(&book); err != nil {
+
+		errorMessages := []string{}
+
+		for _, e := range err.(validator.ValidationErrors) {
+			errorMessage := fmt.Sprintf("Error on field %s, condition %s", e.Field(), e.ActualTag())
+			errorMessages = append(errorMessages, errorMessage)
+		}
+
+		c.JSON(http.StatusBadRequest, gin.H{
+			"errors": errorMessages,
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, book)
+}
