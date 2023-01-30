0ef27efd3207bf34175a752a81b2fd5b0a3fb32e
MirzaHilmi <exquisitemirza@student.ub.ac.id>
2023-01-30 18:38:10 +0700

    add requirement for book so it must be fulfilled first

--- models/item.go
+++ models/item.go
@@ -1,7 +1,7 @@
 package models
 
 type Book struct {
-	Title string `json:"title"`
-	Years uint   `json:"years"`
-	Price uint   `json:"price"`
+	Title string `json:"title" binding:"required"`
+	Years uint   `json:"years" binding:"required, number"`
+	Price uint   `json:"price" binding:"required, number"`
 }
