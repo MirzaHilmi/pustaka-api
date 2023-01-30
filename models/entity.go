ac9c9b22505b8f02061e9ed5d957e17df4cb4f31
MirzaHilmi <exquisitemirza@student.ub.ac.id>
2023-01-31 00:26:16 +0700

    add book struct

--- models/entity.go
+++ models/entity.go
@@ -1,7 +1,19 @@
 package models
 
-type Book struct {
+import "time"
+
+type BookTest struct {
 	Title string      `json:"title" binding:"required"`
 	Years interface{} `json:"years" binding:"required,number"`
 	Price interface{} `json:"price" binding:"required,number"`
+}
+
+type Book struct {
+	ID          int
+	Title       string
+	Description string
+	Price       int
+	Rating      int
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
 }
