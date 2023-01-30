e196fe6dd198f7d277b863f9072116d48f97eae7
MirzaHilmi <exquisitemirza@student.ub.ac.id>
2023-01-30 22:25:42 +0700

    change json number to interface

--- models/item.go
+++ models/item.go
@@ -1,11 +1,7 @@
 package models
 
-import (
-	"encoding/json"
-)
-
 type BookEntity struct {
 	Title string      `json:"title" binding:"required"`
-	Years json.Number `json:"years" binding:"required,number"`
-	Price json.Number `json:"price" binding:"required,number"`
+	Years interface{} `json:"years" binding:"required,number"`
+	Price interface{} `json:"price" binding:"required,number"`
 }
