b8604a2ad63f2104aa8eba9ab845adbf3c2a2417
MirzaHilmi <exquisitemirza@student.ub.ac.id>
2023-01-30 22:19:20 +0700

    move function into util package

--- main.go
+++ main.go
@@ -1,59 +1,17 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-
 	"github.com/gin-gonic/gin"
-	"github.com/go-playground/validator/v10"
-
-	"pustaka-api/models"
+	"pustaka-api/util"
 )
 
 func main() {
 	router := gin.Default()
 
-	router.GET("/", rootHandler)
-	router.GET("/:id", idHandler)
-	router.GET("/book/query", queryHandler) // http://localhost:8080/query?title=Halo
-	router.POST("/book/post", postBookHandler)
+	router.GET("/", util.RootHandler)
+	router.GET("/:id", util.IdHandler)
+	router.GET("/book/query", util.QueryHandler) // http://localhost:8080/query?title=Halo
+	router.POST("/book/post", util.PostBookHandler)
+
 	router.Run()
-}
-
-func rootHandler(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"name": "Mirza Hilmi",
-		"Bio":  "A Software Developer, Backend Developing and Robotics Enthusiast",
-	})
-}
-
-func idHandler(c *gin.Context) {
-	id := c.Param("id")
-	c.JSON(http.StatusOK, gin.H{
-		"id": id,
-	})
-}
-
-func queryHandler(c *gin.Context) {
-	title := c.Query("title")
-	price := c.Query("price")
-
-	c.JSON(http.StatusOK, gin.H{
-		"title": title,
-		"price": price,
-	})
-}
-
-func postBookHandler(c *gin.Context) {
-	var book models.Book
-
-	if err := c.ShouldBindJSON(&book); err != nil {
-		for _, e := range err.(validator.ValidationErrors) {
-			errorMessage := fmt.Sprintf("Error on field %s, condition %s", e.Field(), e.ActualTag())
-			c.JSON(http.StatusBadRequest, errorMessage)
-			return
-		}
-	}
-
-	c.JSON(http.StatusOK, book)
 }
